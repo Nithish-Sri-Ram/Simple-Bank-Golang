122e68b724f7632fd45d7a6fa5b99307d1c11490
Nithish Sri Ram <115037539+Nithish-Sri-Ram@users.noreply.github.com>
2025-01-15 21:59:18 +0530

    Implement login user api with paseto and jwt

--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/Nithish-Sri-Ram/simplebank/db/sqlc"
+	"github.com/Nithish-Sri-Ram/simplebank/token"
+	"github.com/Nithish-Sri-Ram/simplebank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,28 +13,46 @@ import (
 
 // Server serves HTTP requests for our banking service/
 type Server struct {
-	store  db.Store
-	router *gin.Engine // This router will help us send each API request to the correct handler for processing
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine // This router will help us send each API request to the correct handler for processing
 }
 
 // New server creates a new HTTP server and setup routing
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	// Here we've registered the
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
 	router.POST("/accounts", server.createAccount) // We can pass one or multiple handler functions - if we pass multiple functions, then the last one should be the real handler and all the other functions should be middlewares
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
+
 }
 
 // Start runs the http server on a specific address
